b2f359612595c5d2f007c72ea8e4aedb1393703a
sasaokayuzen <56952131+sasaokayuzen@users.noreply.github.com>
2022-10-18 08:08:21 +0900

    Update aerosol.py

--- preprocess/aerosol.py
+++ preprocess/aerosol.py
@@ -6,11 +6,10 @@ import pprint
 
 url = 'https://www.jpmap-jaxa.jp/jpmap/api/v1/rect/'
 
-a = float(input("北側の緯度を入力してください\t>>"))
-b = float(input("西側の経度を入力してください\t>>"))
-c = float(input("南側の緯度を入力してください\t>>"))
-d = float(input("東側の経度を入力してください\t>>"))
-
+lat_max = 34.8
+lon_min = 135.4
+lat_min = 34.4
+lon_max = 135.8
 today = datetime.date.today()
 ty = today.strftime('%Y%m%d')
 
@@ -19,13 +18,24 @@ ly = lastyear.strftime('%Y%m%d')
 
 params = {'product':'4',
           'interval':'3',
-          'nlat': a,
-          'wlon': b,
-          'slat':c,
-          'elon':d,
+          'nlat': lat_max,
+          'wlon': lon_min,
+          'slat':lat_min,
+          'elon':lon_max,
           'start':ly,
           'end': ty }
-
+a = []
 res = requests.get(url, params=params)
-data = json.loads(res.text)
-pprint.pprint(data)
+json_load = json.loads(res.text)
+
+
+for k in range(0,13):
+    if json_load['data'][k]['value'] == -9999:
+        a.append(json_load['data'][k-1]['value'])
+    
+    else :
+        a.append(json_load['data'][k]['value'])
+        
+    k = k + 1
+    
+aerosol_score = a[-1]/0.40
