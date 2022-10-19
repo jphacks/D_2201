22ff07512535438be1d8b351913b2303a1c772e2
sasaokayuzen <56952131+sasaokayuzen@users.noreply.github.com>
2022-10-19 17:27:36 +0900

    Update aerosol.py

--- preprocess/aerosol.py
+++ preprocess/aerosol.py
@@ -2,40 +2,83 @@ import datetime
 from dateutil import relativedelta
 import requests
 import json
-import pprint
+import numpy as np
 
-url = 'https://www.jpmap-jaxa.jp/jpmap/api/v1/rect/'
+poi = [34.4,34.8,135.4,135.8]
+sepi = 50
+di = (poi[1]-poi[0])/sepi
+sepj = 50
+dj = (poi[3]-poi[2])/sepj
 
-lat_max = 34.8
-lon_min = 135.4
-lat_min = 34.4
-lon_max = 135.8
+url = 'https://www.jpmap-jaxa.jp/jpmap/api/v1/rect/' #aerpsplの光学的厚さのリクエストurl
 today = datetime.date.today()
 ty = today.strftime('%Y%m%d')
 
 lastyear = today - relativedelta.relativedelta(years=1)
 ly = lastyear.strftime('%Y%m%d')
 
-params = {'product':'4',
+for i in range(sepi):
+    lat_min = poi[1] - di*(i+1)
+    lat_max = poi[1] - di*i
+    for j in range(sepj):
+        building_score = 0
+        lon_min = poi[2] + dj*j
+        lon_max = poi[2] + dj*(j+1)
+        
+        if lat_min % 0.01 == 0.0:
+            lat_min_round = lat_min
+        else :
+            lat_min_round = round(lat_min,1)
+            
+        if lat_max % 0.01 == 0.0:
+            lat_max_round = lat_max
+        else :
+            lat_max_round = round(lat_max,1)
+            
+        if lat_max_round == lat_min_round :
+            lat_max_round = lat_min_round + 0.1
+            
+
+        if lon_min % 0.01 == 0.0:
+            lon_min_round = lon_min
+        else :
+            lon_min_round = round(lon_min,1)
+            
+        if lon_max % 0.01 == 0.0:
+            lon_max_round = lon_max
+        else :
+            lon_max_round = round(lon_max,1)
+            
+        if lon_max_round == lon_min_round :
+            lon_max_round = lon_min_round + 0.1
+        
+        print('lat_min_round = ', lat_min_round)
+        print('lat_max_round = ', lat_max_round)
+        print('lon_min_round = ', lon_min_round)
+        print('lon_max_round = ', lon_max_round)
+        
+        ##エアロゾルスコア検出
+        params = {'product':'4',
           'interval':'3',
-          'nlat': lat_max,
-          'wlon': lon_min,
-          'slat':lat_min,
-          'elon':lon_max,
+          'nlat': lat_max_round,
+          'wlon': lon_min_round,
+          'slat':lat_min_round,
+          'elon':lon_max_round,
           'start':ly,
           'end': ty }
-a = []
-res = requests.get(url, params=params)
-json_load = json.loads(res.text)
-
-
-for k in range(0,13):
-    if json_load['data'][k]['value'] == -9999:
-        a.append(json_load['data'][k-1]['value'])
-    
-    else :
-        a.append(json_load['data'][k]['value'])
         
-    k = k + 1
-    
-aerosol_score = np.exp( -2.72 * a[-1])
+        a=[]
+        res = requests.get(url, params=params)
+        json_load = json.loads(res.text)
+        for k in range(0,13):
+               
+            if json_load['data'][k]['value'] == -9999:
+                a.append(json_load['data'][k-1]['value'])
+            else :
+                a.append(json_load['data'][k]['value'])   
+            k = k + 1
+            
+        print(a)
+            
+        aerosol_score = np.exp( -2.72 * a[-1])
+        a.clear
