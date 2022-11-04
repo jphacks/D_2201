6e6d63826040b69af308aeeba73f657058deb628
sasaokayuzen <56952131+sasaokayuzen@users.noreply.github.com>
2022-11-05 02:36:49 +0900

    Update starrrrrrr.py

--- preprocess/starrrrrrr.py
+++ preprocess/starrrrrrr.py
@@ -13,14 +13,14 @@ day = datetime.date.today()
 hour = dt_now.hour
 min = dt_now.minute
 
-
-print(day)
-print(hour)
-print(min)
-for e in range(4):
+for i in range(4):
+    print(hour)
     url = 'https://livlog.xyz/hoshimiru/constellation?lat='+str(lat)+'&lng='+str(lng)+'&date='+str(day)+'&hour='+str(hour)+'&min='+str(min)+'&id=&disp='
     res = requests.get(url)
     data = json.loads(res.text)
     json_load = json.loads(res.text)
-    pprint.pprint(data)
+    a = []
+    for j in range(len(json_load['result'])):
+        a.append(json_load['result'][j]['jpName'])
+    print(a)
     hour = hour + 2
